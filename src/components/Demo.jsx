11a9864d233d180a21ff2b96255441d800e4b6fc
Abhinav Jha <abhinavrocx@gmail.com>
2023-04-22 21:00:05 +0530

    feat: API Setup

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
+import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
   const [article, setArticle] = useState({ url: '', summary: ',' });
--- src/services/article.js
+++ src/services/article.js
@@ -1,11 +1,27 @@
 // One specific part of state of global store
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+// hide API Key in environment
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+// Setup API to function properly
 export const articleApi = createApi({
     reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', rapidApiKey);
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+            return headers;
+        }
+    }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => 'test'
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
         })
-    })
+    }),
 });
+
+// ensure API calls are not immediate. Hook is fired on demand when user submits
+export const { useLazyGetSummaryQuery } = articleApi;
